7f7b85d0f0bfe826bfd13e65dc2df846db5d3880
miguel caceres <mdcaceres95@gmail.com>
2021-11-11 21:27:29 -0300

    the left div was filled with home,social,profile,education, skills and tools sections and right div was filled with experience, references, languages and interests sections. was added a anchor tag for go back to the page top and ajs function

--- js/Main.js
+++ js/Main.js
@@ -40,5 +40,14 @@ function scrollActive(){
 }
 window.addEventListener("scroll",scrollActive); 
 
+/*SHOW SCROLL TOP*/
+function scrollTop(){
+    const scrollTop = document.getElementById("scroll-top");
+    if(this.scrollY >= 800) 
+    scrollTop.classList.add("show-scroll"); 
+    else
+    scrollTop.classList.remove("show-scroll")
+}
+window.addEventListener("scroll", scrollTop);
 
 console.log("its working!!!"); 
